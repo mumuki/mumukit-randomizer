a49bd150d3097b2925c828a6b6678fa2ec85faaa
Julián Berbel Alt <julianberbel@hotmail.com>
2018-08-02 12:57:25 -0300

    Add validation to check range values are numeric

--- lib/mumukit/randomizations/range.rb
+++ lib/mumukit/randomizations/range.rb
@@ -1,9 +1,10 @@
 class Mumukit::Randomizer::Randomization::Range < Mumukit::Randomizer::Randomization::Base
   def initialize(lower_bound, upper_bound)
+    raise 'Range bounds must be Numeric' unless lower_bound.is_a?(Numeric) && upper_bound.is_a?(Numeric)
     @choices = Range.new lower_bound, upper_bound
   end
 
   def get(value)
-    modulo(value, choices.first..choices.last)
+    modulo(value, choices)
   end
 end
