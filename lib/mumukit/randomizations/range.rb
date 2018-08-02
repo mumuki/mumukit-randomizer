ca2786a218e5f28b0305e043c0495ac5b50e25d8
Franco Leonardo Bulgarelli <flbulgarelli@users.noreply.github.com>
2018-08-02 18:17:41 -0300

    Style fixes

--- lib/mumukit/randomizations/range.rb
+++ lib/mumukit/randomizations/range.rb
@@ -1,6 +1,7 @@
 class Mumukit::Randomizer::Randomization::Range < Mumukit::Randomizer::Randomization::Base
   def initialize(lower_bound, upper_bound)
-    raise 'Range bounds must be Numeric' unless lower_bound.is_a?(Numeric) && upper_bound.is_a?(Numeric)
+    raise 'lower bound must be Numeric' unless lower_bound.is_a?(Numeric)
+    raise 'upper bound must be Numeric' unless upper_bound.is_a?(Numeric)
     @choices = Range.new lower_bound, upper_bound
   end
 
